6c86acdfec4a905a2acd731ecf4787a7e3e9c83c
Yung Dai <yungchidai@gmail.com>
2019-02-24 19:02:11 -0500

    Initial Commit

---
+++ HydrogenAPIFramework/HydrogenAPIFramework.h
@@ -0,0 +1,19 @@
+//
+//  HydrogenAPIFramework.h
+//  HydrogenAPIFramework
+//
+//  Created by Yung Dai on 2019-02-24.
+//  Copyright © 2019 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HydrogenAPIFramework.
+FOUNDATION_EXPORT double HydrogenAPIFrameworkVersionNumber;
+
+//! Project version string for HydrogenAPIFramework.
+FOUNDATION_EXPORT const unsigned char HydrogenAPIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HydrogenAPIFramework/PublicHeader.h>
+
+
